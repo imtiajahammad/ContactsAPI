12e7572eacfa0470143d33a34345f0a089fc7563
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-08-06 00:01:21 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+/* resources: 
+https://www.youtube.com/watch?v=3NWT9k-6xGg&list=PLfhXFnx72bDVN5plKqXM4X1E76FJvopht&index=25&t=1s&ab_channel=SameerSaini
+*/
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
