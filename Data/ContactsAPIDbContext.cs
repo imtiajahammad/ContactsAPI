49ade0fc6acb34599621a7bec4143167ef90bfc1
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-08-06 10:43:41 +0600

    Create DbContext Class

---
+++ Data/ContactsAPIDbContext.cs
@@ -0,0 +1,19 @@
+
+using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Contact> Contacts { get; set; }
+         
+
+    }
+
+}
+
