0c65c1390f75fa0cae9e49184e89a926c0f615c0
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-08-06 10:44:10 +0600

    Inject DbContext To Services - In Memory Db

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 /* resources: 
 https://www.youtube.com/watch?v=3NWT9k-6xGg&list=PLfhXFnx72bDVN5plKqXM4X1E76FJvopht&index=25&t=1s&ab_channel=SameerSaini
 */
+using ContactsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
 
 var app = builder.Build();
 
